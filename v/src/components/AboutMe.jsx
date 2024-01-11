4298239e7eb9fad0d6246cbf005e983c73a30549
callbackKid <vladislav.yarosh23@gmail.com>
2024-01-11 15:47:29 +0400

    first lesson

---
+++ v/src/components/AboutMe.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const AboutMe = () => {
+  return (
+    <div>
+      <h1>I am frontend developer</h1>
+    </div>
+  )
+}
+
+const AboutMe1 = () => {
+  return (
+    <div>
+      <h1>I am frontend developer</h1>
+    </div>
+  )
+}
+
+export default AboutMe1
