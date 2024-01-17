3bef8c724832fb6664227ce30e6788f41e892615
callbackKid <83603426+callbackKid@users.noreply.github.com>
2024-01-17 23:43:42 +0400

    Create quotes.js

---
+++ src/utils/quotes.js
@@ -0,0 +1,52 @@
+const quotes = [
+  {
+    id: 1,
+    text: 'The only way to do great work is to love what you do.',
+    author: 'Steve Jobs',
+  },
+  {
+    id: 2,
+    text: 'Success is not final, failure is not fatal: It is the courage to continue that counts.',
+    author: 'Winston Churchill',
+  },
+  {
+    id: 3,
+    text: 'Your time is limited, don\'t waste it living someone else\'s life.',
+    author: 'Steve Jobs',
+  },
+  {
+    id: 4,
+    text: 'In three words I can sum up everything I\'ve learned about life: it goes on.',
+    author: 'Robert Frost',
+  },
+  {
+    id: 5,
+    text: 'The future belongs to those who believe in the beauty of their dreams.',
+    author: 'Eleanor Roosevelt',
+  },
+  {
+    id: 6,
+    text: 'The only limit to our realization of tomorrow will be our doubts of today.',
+    author: 'Franklin D. Roosevelt',
+  },
+  {
+    id: 7,
+    text: 'To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.',
+    author: 'Ralph Waldo Emerson',
+  },
+  {
+    id: 8,
+    text: 'Strive not to be a success, but rather to be of value.',
+    author: 'Albert Einstein',
+  },
+  {
+    id: 9,
+    text: 'Don\'t watch the clock; do what it does. Keep going.',
+    author: 'Sam Levenson',
+  },
+  {
+    id: 10,
+    text: 'The best way to predict the future is to create it.',
+    author: 'Peter Drucker',
+  },
+];
