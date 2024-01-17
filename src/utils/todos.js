66a1b900a628ff7118360b9805b2f287b76d1e91
callbackKid <83603426+callbackKid@users.noreply.github.com>
2024-01-17 23:14:04 +0400

    Create todos.js

---
+++ src/utils/todos.js
@@ -0,0 +1,44 @@
+export const todos = [
+  {
+    id: '1',
+    taskTitle: 'Task 1',
+    taskDescription: 'Complete project presentation',
+    doneFlag: false,
+  },
+  {
+    id: '2',
+    taskTitle: 'Task 2',
+    taskDescription: 'Buy groceries',
+    doneFlag: true,
+  },
+  {
+    id: '3',
+    taskTitle: 'Task 3',
+    taskDescription: 'Call mom',
+    doneFlag: false,
+  },
+  {
+    id: '4',
+    taskTitle: 'Task 4',
+    taskDescription: 'Go for a jog',
+    doneFlag: true,
+  },
+  {
+    id: '5',
+    taskTitle: 'Task 5',
+    taskDescription: 'Read a book',
+    doneFlag: false,
+  },
+  {
+    id: '6',
+    taskTitle: 'Task 6',
+    taskDescription: 'Finish coding assignment',
+    doneFlag: true,
+  },
+  {
+    id: '7',
+    taskTitle: 'Task 7',
+    taskDescription: 'Pay bills',
+    doneFlag: false,
+  },
+]
