7cb5ab35f40d6f6730872980b153a043563884cd
callbackKid <vladislav.yarosh23@gmail.com>
2024-02-13 17:22:10 +0400

    dev tools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,7 @@
 import { createStore } from 'redux'
 import { counterReducer } from './CounterReducer'
 
-export const store = createStore(counterReducer)
+export const store = createStore(
+  counterReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
